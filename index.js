339282e580cda5fd67c90d6e7e3b793ab6e42951
Tasnim Jakir <tasnim748@gmail.com>
2023-06-07 16:59:40 +0600

    socket implementation

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ import cors from "cors";
 import { StreamChat } from "stream-chat";
 import { v4 as uuidv4 } from "uuid";
 import bcrypt from "bcrypt";
+import http from "http";
+import { Server } from "socket.io"
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
+
 const api_key = "z86yhp59u5se";
-const api_secret =
-  "re9dju2w5njubrpudwra98e3rbja9f72bnnsad5upc3n52yd4eye9jfp3ta86wna";
+const api_secret = "re9dju2w5njubrpudwra98e3rbja9f72bnnsad5upc3n52yd4eye9jfp3ta86wna";
 const serverClient = StreamChat.getInstance(api_key, api_secret);
 
+
+// for signup
 app.post("/signup", async (req, res) => {
   try {
     const { firstName, lastName, username, password } = req.body;
@@ -24,6 +27,8 @@ app.post("/signup", async (req, res) => {
     res.json(error);
   }
 });
+
+// for login
 app.post("/login", async (req, res) => {
   try {
     const { username, password } = req.body;
@@ -33,7 +38,6 @@ app.post("/login", async (req, res) => {
 
     const token = serverClient.createToken(users[0].id);
     const passwordMatch = await bcrypt.compare(password, users[0].hashedPassword);
-
     
     if (passwordMatch) {
       res.json({
@@ -49,6 +53,25 @@ app.post("/login", async (req, res) => {
   }
 });
 
-app.listen(3002, () => {
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+  cors: {
+      origin: 'http://localhost:3000',
+      methods: ["GET", "POST"],
+  }
+})
+
+io.on("connection", socket => {
+  console.log(`user connected: ${socket.id}`)
+
+  socket.on("game_move", data => {
+      console.log(data)
+      socket.broadcast.emit("move_received", data)
+  })
+})
+
+server.listen(3002, () => {
   console.log("Server is running on port 3002");
 });
